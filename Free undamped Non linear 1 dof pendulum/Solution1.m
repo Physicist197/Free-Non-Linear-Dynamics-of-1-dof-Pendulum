d50c8b98760eae98706ae56cb51f58a625b67fc8
NOTHING <chaureabhimanyu222@gmail.com>
2026-01-11 06:13:03 +0530

    Add comments to Solution1.m for clarity

--- Free undamped Non linear 1 dof pendulum/Solution1.m
+++ Free undamped Non linear 1 dof pendulum/Solution1.m
@@ -1,7 +1,7 @@
-Numerical Solution
+%Numerical Solution
 [t, theta] = ode113('func1', [0, 20], [pi/4, 0]); 
 
-Time series of Angular displacement, Angular velocity, Energy
+%Time series of Angular displacement, Angular velocity, Energy
 plot(t,theta(:, 1))
 grid on
 axis equal
@@ -18,7 +18,7 @@ axis equal
 hold off
 legend("angular displacement", "angular velocity", "Energy")
 
-Phase Space Plot
+%Phase Space Plot
 plot(theta(:, 2), theta(:, 1))
 xlabel("Angular displacement")
 ylabel("Angular velocity")
